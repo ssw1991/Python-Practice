a5bd8be003b734f74236b84676f50690ab38da10
shilowilson31 <shilo.wilson@baruchmail.cuny.edu>
2018-01-17 06:00:29 -0500

    added new code

--- Module 3 - Shilo Wilson/Level 3.1/3.1.1/lambda.py
+++ Module 3 - Shilo Wilson/Level 3.1/3.1.1/lambda.py
@@ -9,7 +9,12 @@ hypotenuseCalc = lambda a, b: (a**2 + b**2)**.5
 
 
 def main():
-    print(hypotenuseCalc(3, 4))
+    txt = 'The hypotenuse of a triangle with ' \
+          'base {} and height {} is {}'
+
+    print txt.format(5, 6, hypotenuseCalc(5, 6))
+    print txt.format(14, 32, hypotenuseCalc(14, 32))
+    print txt.format(8, 9, hypotenuseCalc(8, 9))
 
 
 if __name__ == '__main__':
